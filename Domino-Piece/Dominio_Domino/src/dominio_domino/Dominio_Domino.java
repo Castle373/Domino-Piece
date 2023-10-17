383513f3c189581ba5f87274de2544be035eac40
IvanBus233188 <ivan.bustillos233188@potros.itson.edu.mx>
2023-10-16 18:12:23 -0600

    Prueba commits
    Prueba de commits

---
+++ Domino-Piece/Dominio_Domino/src/dominio_domino/Dominio_Domino.java
@@ -0,0 +1,17 @@
+
+package dominio_domino;
+
+/**
+ *
+ * @author IVAN, Diego, CORNEJO, Marcos
+ */
+public class Dominio_Domino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
